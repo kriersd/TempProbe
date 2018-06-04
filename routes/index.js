8c8dd1ebc39527d15a69fba5b6b4b6b46178a231
DAVID KRIER <dkflyboy@gmail.com>
2018-06-04 18:31:26 -0500

    Working Gague

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // Do stuff here
+    //deviceID & token are URL params for my particle device, which are passed in on the URL string.
+    res.render('index', { deviceID: req.param("deviceID"), token: req.param("token") });
+
 });
 
 module.exports = router;
